10bcb49fce56ad8eea3feeabaa9fe7a5a1e3c7c2
pintor1331 <ohlit12@gmail.com>
2023-03-17 13:31:53 +0900

    pintor #4

--- src/main/java/org/example/pintor.java
+++ src/main/java/org/example/pintor.java
@@ -5,6 +5,6 @@ public class pintor {
         System.out.println("First commit from pintor.");
         System.out.println("Second commit from pintor.");
         System.out.println("Third commit from pintor.");
-
+        System.out.println("Fourth commit from pintor.");
     }
 }
