2acdf8494c2af6616220a79884f4a70b66179ea9
MunChangbin <vv4704@gmail.com>
2023-03-17 13:32:20 +0900

    변경

---
+++ src/main/java/org/example/cb.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class cb {
+    public static void main(String[] args) {
+        System.out.println("안녕 난 창빈이야");
+        System.out.println("자바 어려워");
+        System.out.println("git 이 뭐야?");
+        System.out.println("슬프다.");
+
+    }
+
+}
