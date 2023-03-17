ffdd0a224a3f8b8c5dcc0d805564641149ebaf08
pintor1331 <ohlit12@gmail.com>
2023-03-17 13:31:34 +0900

    pintor #3

--- src/main/java/org/example/pintor.java
+++ src/main/java/org/example/pintor.java
@@ -4,6 +4,7 @@ public class pintor {
     public static void main(String[] args) {
         System.out.println("First commit from pintor.");
         System.out.println("Second commit from pintor.");
+        System.out.println("Third commit from pintor.");
 
     }
 }
