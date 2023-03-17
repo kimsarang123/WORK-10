5c2b3e16ca0fca1007fe583995bd04917fcb6de3
thdcodud01 <thdcodud01@gmail.com>
2023-03-17 13:31:29 +0900

    변경

--- src/main/java/org/example/cy.java
+++ src/main/java/org/example/cy.java
@@ -4,5 +4,6 @@ public class cy {
     public static void main(String[] args) {
         System.out.println("ha............................");
         System.out.println("hu............................");
+        System.out.println("ho............................");
     }
 }
